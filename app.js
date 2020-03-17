a9a459f65e153da31b65058515166d5ef2a1d930
devcesar <cesarmiguel0516@hotmail.com>
2020-03-17 13:12:20 -0500

    config. cabezeras

--- app.js
+++ app.js
@@ -2,12 +2,33 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const routes = express.Router();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+app.use(cors());
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
 routes.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
+app.use((req, res, next) => {
+    //configurar cabecera
+    //para permitir el acceso a nuestra api de todos los dominios
+    res.header('Access-Control-Allow-Origin', '*');
+    //cabeceras necesarias para AJAX
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
 
+    //para salir del flujo y seguir
+    next();
+});
 
 
 app.use('/', routes);
