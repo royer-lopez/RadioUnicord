834d8d0edeab33d3a962e7c5f4c863f7d3d65a6d
devcesar <cesarmiguel0516@hotmail.com>
2020-03-17 14:16:54 -0500

    version 1.0

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const routes = express.Router();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
+app.use(express.static(__dirname + '/public'));
 app.use(cors());
-
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({
 }));
 
 routes.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/public/index.html'));
+    res.sendFile(path.join(__dirname + '/public'));
 });
 
 app.use((req, res, next) => {
